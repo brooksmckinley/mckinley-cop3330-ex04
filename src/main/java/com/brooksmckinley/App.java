a87543aa89cf15c4de9e96c6e7e392cb97f44e17
Brooks McKinley <brooksmckinley@yahoo.com>
2021-09-13 17:47:19 -0400

    Implement tests and logic

--- src/main/java/com/brooksmckinley/App.java
+++ src/main/java/com/brooksmckinley/App.java
@@ -2,4 +2,11 @@ package com.brooksmckinley;
 
 public class App {
 
+    public static void main(String[] args) {
+
+    }
+
+    public static String madlib(String noun, String verb, String adjective, String adverb) {
+        return "Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!";
+    }
 }
--- src/test/java/com/brooksmckinley/AppTest.java
+++ src/test/java/com/brooksmckinley/AppTest.java
@@ -1,5 +1,21 @@
 package com.brooksmckinley;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
 
+    @Test
+    public void testExampleMadLib() {
+        String expected = "Do you walk your blue dog quickly? That's hilarious!";
+        String actual = App.madlib("dog", "walk", "blue", "quickly");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAnotherMadLib() {
+        String expected = "Do you eat your green cow slowly? That's hilarious!";
+        String actual = App.madlib("cow", "eat", "green", "slowly");
+        Assertions.assertEquals(expected, actual);
+    }
 }
